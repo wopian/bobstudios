abccb139ddefea14ee6958e88fd229963cd29392
James <wopian@wopian.me>
2017-01-25 01:02:24 +0000

    rebuild pages at 31707e3

--- README.md
+++
@@ -1,2 +0,0 @@
-# bobstudios
-Repo for Bobstudios' website
--- build/gh-pages.js
+++
@@ -1,5 +0,0 @@
-const gulp   = require('gulp'),
-      deploy = require('gulp-gh-pages');
-
-gulp.task('deploy', () => gulp.src('./dist/**/*')
-  .pipe(deploy()));
